c609da19e4a8a6ec6225af343cd13ebafe740d23
renaldysab18 <renaldysab18@gmail.com>
2020-05-25 15:38:08 +0700

    [add] adding detail screen

---
+++ lib/ui/detail_news_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailNewsScreen extends StatefulWidget {
+  @override
+  _DetailNewsScreenState createState() => _DetailNewsScreenState();
+}
+
+class _DetailNewsScreenState extends State<DetailNewsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
